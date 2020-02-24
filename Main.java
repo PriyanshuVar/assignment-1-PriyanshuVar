4245e092c4d01008f2bdd66462d035ed5ddf5645
PriyanshuVar <43368045+PriyanshuVar@users.noreply.github.com>
2020-02-24 19:13:01 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+package src.Assignemnt;
+
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        LinkedList n2 = new LinkedList();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+        while (flag == true) {
+            System.out.println("Welcome to AG's Contact List App");
+            System.out.println("Press 1 to add a new contact");
+            System.out.println("Press 2 to view all contacts");
+            System.out.println("Press 3 to delete a contact");
+            System.out.println("Press 4 to exit program");
+            System.out.println();
+            int n = sc.nextInt();
+            sc.nextLine();
+            
+        }
+    }
+}
